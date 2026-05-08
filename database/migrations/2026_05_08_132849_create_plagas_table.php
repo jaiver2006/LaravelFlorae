59c87567fef19751a316e7af6d7a7ea892a9be91
nikoll Guaca <guacanikoll@gmail.com>
2026-05-08 09:13:59 -0500

    creación tabla plagas

--- database/migrations/2026_05_08_132849_create_plagas_table.php
+++ database/migrations/2026_05_08_132849_create_plagas_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('plagas', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('Description');
+            $table->text('Symptoms');
+            $table->text('Treatment');
             $table->timestamps();
         });
     }
