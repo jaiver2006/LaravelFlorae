8b58503e670288539712e9a0ee1fef6641328f1f
jaiver quintero <jaiverteran664@gmail.com>
2026-05-08 08:51:44 -0500

    se añadieromn atributos a la tabla Jardins

--- database/migrations/2026_05_08_132655_create_jardins_table.php
+++ database/migrations/2026_05_08_132655_create_jardins_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('jardins', function (Blueprint $table) {
             $table->id();
+            $table->string('PlantClassification');
+            $table->integer('Plant Quantity');
+            $table->date('CreationDate');
             $table->timestamps();
         });
     }
